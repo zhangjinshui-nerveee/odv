f412ea17cd00af9d08d94259e11283d06561eecd
zhangjinshui <jinshui-account@proton.me>
2025-05-05 22:14:24 -0400

    upload raw data; add milestones

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# import matplotlib.pyplot as plt
+
+'''
+MILESTONES
+- [ ] read data with pandas
+- [ ] plot data out, adjust the format to match the appearance of oscilloscope, show only 10,000 data points (equally spreaded)
+- [ ] use packages of tkinter or dash to embed the plot in a user-interfacing frame
+- [ ] add button to allow spread signals into different channels, e.g. we have three signals in tek5494, the toggle button to allow show them on the same plot or three separate ones
+- [ ] add function to allow select x-axis range
+- [ ] change the input approach of selecting x-range to mouse-dragging
+'''
