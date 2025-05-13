5e005a6988f7bc7f5abd3b1e0a01a40c21665355
Ian Le <ianlllym@gmail.com>
2025-05-13 01:32:57 -0700

    Add plot_combined()

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 '''
 MILESTONES
 - [x] read data with pandas
-- [ ] plot data out, adjust the format to match the appearance of oscilloscope, show only 10,000 data points (equally spreaded)
+- [x] plot data out, adjust the format to match the appearance of oscilloscope, show only 10,000 data points (equally spreaded)
 - [ ] use packages of tkinter or dash to embed the plot in a user-interfacing frame
 - [ ] add button to allow spread signals into different channels, e.g. we have three signals in tek5494, the toggle button to allow show them on the same plot or three separate ones
 - [ ] add function to allow select x-axis range
@@ -30,6 +30,47 @@ def downsample(df: pd.DataFrame, n: int) -> pd.DataFrame:
     downsampled_df = df.iloc[indices]
     return downsampled_df
 
+def plot_combined(df: pd.DataFrame):
+    """Plot the given df such that all channels are combined in one plot"""
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=df["TIME"],
+        y=df["CH1"],
+        mode="lines",
+        name="CH1",
+        line=dict(color="yellow")
+    ))
+    fig.add_trace(go.Scatter(
+        x=df["TIME"],
+        y=df["CH2"],
+        mode="lines",
+        name="CH2",
+        line=dict(color="cyan")
+    ))
+    fig.add_trace(go.Scatter(
+        x=df["TIME"],
+        y=df["CH4"],
+        mode="lines",
+        name="CH4",
+        line=dict(color="lime")
+    ))
+
+    fig.update_layout(
+        template="plotly_dark",
+        title=dict(text="Oscilloscope Data"),
+        xaxis=dict(
+            griddash="dot",
+            zeroline=False
+        ),
+        yaxis=dict(
+            griddash="dot",
+            zeroline=False
+        ),
+    )
+
+    fig.show()
+
 if __name__ == "__main__":
     df = read_data("tek5494.csv")
     print(df.head())
@@ -39,3 +80,5 @@ if __name__ == "__main__":
     df = downsample(df, 10_000)
     print(df.head())
     print(len(df))
+
+    plot_combined(df)
